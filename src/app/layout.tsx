a37c4e0c9e95c2ba18089af397222641ec70c442
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-01 17:28:22 +0330

    ✨feat: add dark/light theme provider and mode toggle components with dropdown menu for theme selection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 
+import { ThemeProvider } from '@/components/theme-provider';
+
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -20,7 +22,16 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
         />
       </head>
-      <body className="h-screen w-screen">{children}</body>
+      <body className="h-screen w-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
