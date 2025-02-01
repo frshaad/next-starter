c264648284686130fe465aba85c62f3f793ee0b6
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-01 18:42:26 +0330

    ✨feat: update environment schema and example file to include AUTH_DRIZZLE_URL and new dependencies

--- src/lib/env/schema.ts
+++ src/lib/env/schema.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
+  // Next-Auth environment variables
   AUTH_SECRET: z.string(),
   AUTH_GOOGLE_ID: z.string(),
   AUTH_GOOGLE_SECRET: z.string(),
+  AUTH_DRIZZLE_URL: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
