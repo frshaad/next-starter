1ecd58bafd7f02b4360fd73a2ccb01eaff5b40ef
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-01 17:53:08 +0330

    ✨feat: add error, loading, and not found components

---
+++ src/app/error.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex size-full items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>Error ❌</CardTitle>
+          <CardDescription>Something went wrong!</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Button onClick={() => reset()} className="w-full">
+            Try again
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
