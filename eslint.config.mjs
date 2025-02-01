21abf5749b2cd3bfc033678043b9af327fdebab0
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-01 17:12:46 +0330

    feat: update Prettier and ESLint configurations, enhance VSCode settings, and refactor layout and styles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import checkFile from 'eslint-plugin-check-file';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -7,10 +8,29 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
-    plugins: {},
+    plugins: {
+      'check-file': checkFile,
+    },
     rules: {
       'prefer-arrow-callback': ['error'],
       'prefer-template': ['error'],
+      'no-useless-rename': [
+        'error',
+        {
+          ignoreDestructuring: false,
+          ignoreImport: false,
+          ignoreExport: false,
+        },
+      ],
+      'check-file/filename-naming-convention': [
+        'error',
+        { '**/*.{ts,tsx}': 'KEBAB_CASE' },
+        { ignoreMiddleExtensions: true },
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        { 'src/**/!(__tests__)': 'KEBAB_CASE' },
+      ],
     },
   },
 ];
