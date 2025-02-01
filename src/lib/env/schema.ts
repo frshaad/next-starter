a693a965d09cd1bbe067f7ce0153321f61f69545
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-01 18:32:24 +0330

    ✨feat: add next-auth dependency and update environment schema to include AUTH_SECRET

--- src/lib/env/schema.ts
+++ src/lib/env/schema.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
-export const envSchema = z.object({});
+export const envSchema = z.object({
+  AUTH_SECRET: z.string(),
+});
 
 export type Env = z.infer<typeof envSchema>;
