613d73798c6aa990c805636ada5074ba79417e30
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-01 16:09:42 +0330

    feat: add VSCode settings for formatting and update Prettier and ESLint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: {},
+    rules: {
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+    },
+  },
 ];
 
 export default eslintConfig;
