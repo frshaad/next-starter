1a1876aa835a38c53454a5691ab5d8b8f412d54f
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-02-28 20:00:35 +0330

    🔧 update Prettier configuration and ESLint rules for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,6 @@ import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import configPrettier from 'eslint-config-prettier';
 import n from 'eslint-plugin-n';
-import tailwind from 'eslint-plugin-tailwindcss';
-import unicorn from 'eslint-plugin-unicorn';
 import tseslint from 'typescript-eslint';
 
 const MAX_DEPTH = 4;
@@ -18,52 +16,37 @@ export default tseslint.config([
   { languageOptions: { globals: { React: true } }, ignores: ['node_modules'] },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   js.configs.recommended,
-  unicorn.configs.recommended,
-  ...tailwind.configs['flat/recommended'],
   {
     plugins: { n },
     rules: {
-      'prefer-const': 'error',
       'max-depth': ['warn', { max: MAX_DEPTH }],
       'max-params': ['error', { max: MAX_FN_PARAMS }],
-      'no-var': 'error',
-      'no-use-before-define': 'error',
+      'prefer-const': 'error',
       'no-eval': 'error',
       'no-param-reassign': 'error',
+      'no-promise-executor-return': 'error',
+      'no-unreachable-loop': 'error',
+
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-import-type-side-effects': 'error',
       '@typescript-eslint/no-non-null-assertion': 'error',
       '@typescript-eslint/prefer-as-const': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+
       'react/button-has-type': 'error',
       'react/jsx-no-constructed-context-values': 'error',
       'react/no-array-index-key': 'error',
       'react/self-closing-comp': 'error',
-      'jsx-a11y/lang': 'error',
-      'jsx-a11y/no-static-element-interactions': 'error',
-      'n/no-process-env': 'error',
-      'unicorn/prevent-abbreviations': [
-        'error',
-        {
-          allowList: {
-            util: true,
-            utils: true,
-            prop: true,
-            props: true,
-            param: true,
-            params: true,
-            env: true,
-            db: true,
-          },
-        },
-      ],
-      'no-promise-executor-return': 'error',
-      'no-unreachable-loop': 'error',
-      '@typescript-eslint/no-explicit-any': 'warn',
       'react/jsx-no-useless-fragment': 'error',
       'react/hook-use-state': 'error',
       'react/no-unstable-nested-components': 'error',
+
+      'jsx-a11y/lang': 'error',
+      'jsx-a11y/no-static-element-interactions': 'error',
       'jsx-a11y/click-events-have-key-events': 'error',
       'jsx-a11y/no-noninteractive-element-interactions': 'error',
+
+      'n/no-process-env': 'error',
     },
   },
   configPrettier,
