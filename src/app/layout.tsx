c492d1b313c43843b6f9d199bcd771b097ff2d34
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-03-07 21:39:32 +0330

    🔧 remove unused favicon link from RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
-          rel="icon"
-        />
-      </head>
       <ReactScan />
       <body className="h-screen w-screen">
         <ThemeProvider
