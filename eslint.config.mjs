48546d18b144ee0503608d6390fae19f7f5d1ac1
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-05-07 16:35:04 +0330

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,35 @@
 import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
 import configPrettier from 'eslint-config-prettier'
 import n from 'eslint-plugin-n'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import eslintPluginUnicorn from 'eslint-plugin-unicorn'
-import tseslint from 'typescript-eslint'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const MAX_DEPTH = 4
 const MAX_FN_PARAMS = 3
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 })
 
-export default tseslint.config([
+export default defineConfig([
   { ignores: ['node_modules'], languageOptions: { globals: { React: true } } },
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  js.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  eslintPluginUnicorn.configs.recommended,
-  {
-    plugins: { n, 'simple-import-sort': simpleImportSort },
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/no-deprecated': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-import-type-side-effects': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/prefer-as-const': 'error',
 
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  // JavaScript
+  js.configs.recommended,
+  {
+    rules: {
       'consistent-return': 'error',
       curly: 'error',
-
-      'jsx-a11y/click-events-have-key-events': 'error',
-      'jsx-a11y/lang': 'error',
-      'jsx-a11y/no-noninteractive-element-interactions': 'error',
-      'jsx-a11y/no-static-element-interactions': 'error',
-
       'max-depth': ['error', { max: MAX_DEPTH }],
       'max-params': ['error', { max: MAX_FN_PARAMS }],
-
-      'n/no-process-env': 'error',
-
       'no-duplicate-imports': 'error',
       'no-eval': 'error',
       'no-lonely-if': 'error',
@@ -60,22 +39,58 @@ export default tseslint.config([
       'no-unreachable-loop': 'error',
       'no-var': 'error',
       'object-shorthand': 'error',
-
       'prefer-const': 'error',
       'prefer-template': 'error',
+    },
+  },
 
+  // TypeScript
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/prefer-as-const': 'error',
+    },
+  },
+
+  // React
+  {
+    rules: {
       'react/button-has-type': 'error',
       'react/jsx-no-constructed-context-values': 'error',
       'react/jsx-no-useless-fragment': 'error',
       'react/no-array-index-key': 'error',
       'react/no-unstable-nested-components': 'error',
-
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
-
-      'unicorn/no-null': 'off',
-      'unicorn/prevent-abbreviations': 'off',
     },
   },
+
+  // JSX A11y
+  {
+    rules: {
+      'jsx-a11y/click-events-have-key-events': 'error',
+      'jsx-a11y/lang': 'error',
+      'jsx-a11y/no-noninteractive-element-interactions': 'error',
+      'jsx-a11y/no-static-element-interactions': 'error',
+    },
+  },
+
+  {
+    plugins: { n },
+    rules: {
+      'n/no-process-env': 'error',
+    },
+  },
+
+  {
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+    },
+  },
+
   configPrettier,
 ])
