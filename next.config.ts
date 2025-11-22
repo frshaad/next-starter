69c1b1e8cf2e801dfced9be3e8320b139fc1ce3b
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-11-22 11:38:10 +0330

    update next.config.ts to add support for typedRoutes, logging fetch urls, viewTransition, webVitalsAttribution

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  typedRoutes: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    viewTransition: true,
+    webVitalsAttribution: ['CLS', 'LCP', 'INP'],
+  },
 };
 
 export default nextConfig;
